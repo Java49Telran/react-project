c9cbdcbbceffb9635e969fb21249c5df88d146b7
Yuragranovsky <yuragranovsky@gmail.com>
2023-02-19 15:48:04 +0200

    HW #42 Statistics DataGrid

--- src/components/pages/AgeStatistics.tsx
+++ src/components/pages/AgeStatistics.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Box, Typography} from "@mui/material";
-import { statAge } from '../../service/EmployeesService';
 import {useSelector} from 'react-redux';
 import { Employee } from '../../model/Employee';
+import { Statistics } from '../Statistics';
 export const AgeStatistics: React.FC = () => {
     const employees: Employee[] = useSelector<any, Employee[]>(state => state.company.employees)
-    return <Box>
-        <Typography>{JSON.stringify(statAge(employees))}</Typography>
-    </Box>
+    const employeesAge = employees.map(empl => ({
+        ...empl, age: new Date().getFullYear() - new Date(empl.birthDate).getFullYear()
+    }))
+    return <Statistics title="Age Statistics" field="age" objects={employeesAge} />
 }
--- src/components/pages/SalaryStatistics.tsx
+++ src/components/pages/SalaryStatistics.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import {Box, Typography} from "@mui/material";
-import { statSalary } from '../../service/EmployeesService';
+
 import {useSelector} from 'react-redux';
 import { Employee } from '../../model/Employee';
+import { Statistics } from '../Statistics';
 export const SalaryStatistics: React.FC = () => {
     const employees = useSelector<any, Employee[]>
     (state => state.company.employees)
-    return <Box>
-        <Typography>{JSON.stringify(statSalary(employees))}</Typography>
-    </Box>
+    return <Statistics title="Salary Statistics" field="salary" objects={employees}/>
 }
